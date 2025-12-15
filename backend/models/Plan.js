3e19ca128b4d626192033c26dc3129b7e349f04c
Cryptexfx-web <4kccfdvzzm@privaterelay.appleid.com>
2025-12-15 21:20:13 +0100

    Delete backend directory

--- backend/models/Plan.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports = mongoose.model("Plan", new mongoose.Schema({
-  name: String,
-  min: Number,
-  max: Number,
-  dailyROI: Number,
-  duration: Number
-}));
